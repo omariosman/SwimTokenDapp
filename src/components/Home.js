6db9933d9c3ff1ed8e5a7638bbc63bf8c078de51
KareemiKassab <57846377+kareemikassab7@users.noreply.github.com>
2023-03-01 08:56:43 +0200

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -152,7 +152,7 @@ const Home = () => {
     }
   };
 
-  const THREE_DAYS_IN_MS = 27 * 24 * 60 * 60 * 1000;
+  const THREE_DAYS_IN_MS = 19 * 24 * 60 * 60 * 1000;
   const NOW_IN_MS = new Date().getTime();
   const dateTimeAfterThreeDays = NOW_IN_MS + THREE_DAYS_IN_MS;
 
